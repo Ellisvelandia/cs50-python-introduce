02cf15fd0dd93357850a89dd50a8f7d563933a0f
Ellisvelandia <eyis619l@gmail.com>
2023-07-30 21:04:29 -0500

    string methods

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
-name = input("whats your name? ")
-print("welcome " + name)
+#Ask user for their name
+name = input("whats your name? ").strip().title()
+
+#remove whitespace from str
+# name = name.strip().title()
+
+#Capitalize user's name 
+
+# name = name.capitalize()
+# name = name.title()
+
+print("welcome " + name + " your awesome and very smart")
+
 age = input("what is your age? ")
-print(int(age))
+print(int(age) , str("your age is great"))
